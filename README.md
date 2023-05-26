d91d6d2bef7258ec51c51b5bc632d6edab0d4f92
kamalesh5 <kamaleshgok007@gmail.com>
2023-05-26 16:48:52 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,28 @@
+
+# ABOUT THIS APP
+
+This App is the inspired version of Youtube Application. This application has built with attractive UI and fully optimized for user experience. It uses real data by leveraging the Rapid API's youtube API. HomePage displays the most popular videos and has options for the wishlist to watch our favourite categories of videos. 
+
+## WHAT DOES THIS APP DO?
+
+It is an inspired version of Youtube. It has a fully-fledged working functionality such as Home Page, Video Details, Channel Details, and search functionality.
+
+## USED TECHNOLOGIES
+
+1. HTML
+2. CSS
+3. MATERIAL UI
+4. JAVASCRIPT
+5. REACT JS
+6. AXIOS
+
+## FEATURES
+
+1. HOME PAGE WITH VARIOUS CATEGORIES AND OPTIONS.
+2. VIDEO DETAILS AND PLAYER PAGE BY CLICKING THE VIDEOS.
+3. CHANNEL DETAILS PAGE BY CLICKING THE CHANNEL.
+4. COMPLETE SEARCH FUNCTIONALITY, SUCH AS SEARCH BY VIDEO TITLE, DESCRIPTION OR BY CHANNEL NAME.
+
+### DISCLAIMER
+
+This project is solely intended for educational purposes and is created under fair use.
---
+++ src/utils/constants.js
@@ -0,0 +1,42 @@
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import HomeIcon from '@mui/icons-material/Home';
+import CodeIcon from '@mui/icons-material/Code';
+import OndemandVideoIcon from '@mui/icons-material/OndemandVideo';
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+import LiveTvIcon from '@mui/icons-material/LiveTv';
+import SchoolIcon from '@mui/icons-material/School';
+import FaceRetouchingNaturalIcon from '@mui/icons-material/FaceRetouchingNatural';
+import CheckroomIcon from '@mui/icons-material/Checkroom';
+import GraphicEqIcon from '@mui/icons-material/GraphicEq';
+import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+import DeveloperModeIcon from '@mui/icons-material/DeveloperMode';
+
+export const logo = 'https://i.ibb.co/s9Qys2j/logo.png';
+
+export const categories = [
+  { name: 'New', icon: <HomeIcon />, },
+  { name: 'WWE', icon: <LiveTvIcon />, },
+  { name: 'Coding', icon: <CodeIcon />, },
+  { name: 'ReactJS', icon: <CodeIcon />, },
+  { name: 'NextJS', icon: <CodeIcon />, },
+  { name: 'Music', icon: <MusicNoteIcon /> },
+  { name: 'Education', icon: <SchoolIcon />, },
+  { name: 'Podcast', icon: <GraphicEqIcon />, },
+  { name: 'Movie', icon: <OndemandVideoIcon />, },
+  { name: 'Gaming', icon: <SportsEsportsIcon />, },
+  { name: 'Live', icon: <LiveTvIcon />, },
+  { name: 'Sport', icon: <FitnessCenterIcon />, },
+  { name: 'Fashion', icon: <CheckroomIcon />, },
+  { name: 'Beauty', icon: <FaceRetouchingNaturalIcon />, },
+  { name: 'Comedy', icon: <TheaterComedyIcon />, },
+  { name: 'Gym', icon: <FitnessCenterIcon />, },
+  { name: 'Crypto', icon: <DeveloperModeIcon />, },
+];
+
+export const demoThumbnailUrl = 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.gstatic.com%2Fyoutube%2Fimg%2Fbranding%2Fyoutubelogo%2Fsvg%2Fyoutubelogo.svg&imgrefurl=https%3A%2F%2Fwww.youtube.com%2Fhowyoutubeworks%2Fpolicies%2Fcommunity-guidelines%2F&tbnid=mJe9Z8aqJSzHdM&vet=12ahUKEwj-0a75hbj8AhUsi9gFHXrBAicQMygDegUIARDkAQ..i&docid=jYNy5eYduWWhHM&w=800&h=179&q=youtube&hl=en-US&ved=2ahUKEwj-0a75hbj8AhUsi9gFHXrBAicQMygDegUIARDkAQ';
+export const demoChannelUrl = '/channel/UCmXmlB4-HJytD7wek0Uo97A';
+export const demoVideoUrl = '/video/9FDuUd39NOI';
+export const demoChannelTitle = 'Demo Title';
+export const demoVideoTitle = 'Hi there, this is my Youtube Clone!';
+export const demoProfilePicture = 'http://dergipark.org.tr/assets/app/images/buddy_sample.png'
